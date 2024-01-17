a32f467813a9ac96e97956d10ec569be9c47467a
PhannitaSaisorn <pang.pntt@gmail.com>
2024-01-18 02:08:40 +0700

    sidebar

--- SVAutotire/src/App.vue
+++ SVAutotire/src/App.vue
@@ -1,74 +1,38 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div id="app">
+    <header>
+      <Sidebar/>
+    </header>
+    <router-view :key="$route.fullPath" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+
+export default {
+  data () {
+    return {
+      user: null
+    }
+  },
+     mounted () {
+     this.onAuthChange()
+   },
+   methods: {
+     onAuthChange () {
+       const token = localStorage.getItem('token')
+       if (token) {
+         this.getUser()
+       }
+     },
+     getUser () {
+       const token = localStorage.getItem('token')
+      console.log(token)
+     },
+   }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
---
+++ SVAutotire/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+import
+<template lang="">
+    <div class="p-4 sm:ml-64">
+        <h1>Hello</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
