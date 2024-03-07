a99deb74dedfbf5c3b199ad52bb35ad00c44e4ad
Captain <archan.phalanuphat@gmail.com>
2024-03-07 22:55:45 +0700

    edit

--- SVAutotire/src/components/TableOrder.vue
+++ SVAutotire/src/components/TableOrder.vue
@@ -1,6 +1,6 @@
 <template lang="">
       <div class="relative overflow-x-auto sm:rounded-lg">
-      <!-- <div class="flex items-center justify-end md:space-y-0 pb-4 mt-5 bg-white">
+      <div class="flex items-center justify-end md:space-y-0 pb-4 mt-5 bg-white">
         <div class="relative">
           <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg class="w-4 h-4 mx-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -17,8 +17,7 @@
           </div>
           <input v-model="searchItemName" type="text" id="table-search-users" class="block p-2 ps-10 mx-4 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50" placeholder="ค้นหาชื่อลูกค้า">
         </div>
-
-      </div> -->
+      </div>
       <table class="w-full text-left rtl:text-right text-gray-500 mt-10">
         <thead class="text-base text-gray-700 uppercase bg-gray-100 ">
           <tr>
@@ -32,13 +31,30 @@
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b" v-for="(item, index) in filteredItems" :key="index">
-            <td v-for="(value, key) in item" :key="key" class="px-7 py-4">
+          <tr class="bg-white border-b" v-for="(item, index) in items" :key="index" value="item.ID">
+            <td class="px-7 py-4">
+              {{item.ID}}
               <div v-if="editMode && index === editItemIndex">
-                <input v-model="editedItem[key]" class="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-24" :placeholder="key">
+                <input v-model="editedItem.ID" class="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-24" :placeholder="key">
               </div>
               <div v-else class="font-normal text-gray-500">{{ value }}</div>
             </td>
+            <td class="px-7 py-4">
+              {{item.customerName}}
+              <div v-if="editMode && index === editItemIndex">
+                <input v-model="editedItem.customerName" class="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-24" :placeholder="key">
+              </div>
+              <div v-else class="font-normal text-gray-500">{{ value }}</div>
+            </td>
+
+            <td class="px-7 py-4">
+              {{item.ID}}
+              <div v-if="editMode && index === editItemIndex">
+                <input v-model="editedItem.ID" class="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg w-24" :placeholder="key">
+              </div>
+              <div v-else class="font-normal text-gray-500">{{ value }}</div>
+            </td>
+
             <td class="px-7 py-4">
               <a v-if="!editMode" @click="editItem(index)" href="#" class="font-medium text-blue-600 hover:underline">แก้ไข</a>
               <a v-else @click="saveItem(index)" href="#" class="font-medium text-green-600 hover:underline">บันทึก</a>
@@ -54,8 +70,9 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    data() {
+  data() {
     return {
       currentWarehouse: null,
       searchItem: "",
@@ -63,36 +80,7 @@ export default {
       editMode: false,
       editItemIndex: null,
       editedItem: {},
-      items: [
-        {
-          orderID: "1002321",
-          customer: 'พัณณิตา',
-          productID: "ล้อแม็ก",
-          product: "ล้อ",
-          amount: "100",
-        },
-        {
-            orderID: "10024",
-            customer: 'พัณณิตา',
-            productID: "ล้อแม็ก",
-            product: "ล้อ",
-            amount: "100",
-        },
-        {
-            orderID: "1002321",
-          customer: 'พัณณิตา',
-          productID: "ล้อแม็ก",
-          product: "ล้อ",
-          amount: "100",
-        },
-        {
-            orderID: "1002321",
-          customer: 'พัณณิตา',
-          productID: "ล้อแม็ก",
-          product: "ล้อ", 
-          amount: "100",
-        }
-      ],
+      items: [],
     };
   },
   computed: {
@@ -103,7 +91,7 @@ export default {
         return orderIDMatch || customerMatch;
       });
     },
-},
+  },
   methods: {
     editItem(index) {
       this.editMode = true;
@@ -119,11 +107,16 @@ export default {
     deleteItem(index) {
       this.items.splice(index, 1);
     },
+    async getOrder() {
+      const order = await axios.get('http://localhost:3000/saleOrder/all');
+      console.log(order)
+      this.items = order.data.data
+    }
   },
-
+  async mounted() {
+    await this.getOrder();
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
