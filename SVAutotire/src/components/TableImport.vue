c233880cd88d6e86e25b48e1eabe3ca2c184bfcf
Captain <archan.phalanuphat@gmail.com>
2024-03-12 23:36:56 +0700

    edit

--- SVAutotire/src/components/TableImport.vue
+++ SVAutotire/src/components/TableImport.vue
@@ -3,6 +3,7 @@
         <table class="w-full text-left rtl:text-right text-gray-500 mt-10">
             <thead class="text-base text-gray-700 uppercase bg-gray-100 ">
                 <tr>
+                    <th scope="col" class="px-6 py-3 font-bold">วันที่</th>
                     <th scope="col" class="px-6 py-3 font-bold">ชนิดสินค้า</th>
                     <th scope="col" class="px-6 py-3 font-bold">รหัสสินค้า</th>
                     <th scope="col" class="px-6 py-3 font-bold">บริษัท</th>
@@ -11,10 +12,12 @@
             </thead>
             <tbody>
                 <tr v-for="(item, index) in items" :key="index">
+                    <td>{{ item.date }}</td>
                     <td>{{ item.type }}</td>
                     <td>{{ item.id }}</td>
                     <td>{{ item.company }}</td>
                     <td>{{ item.amount }}</td>
+
                 </tr>
             </tbody>
         </table>
@@ -22,15 +25,32 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    items: [
-        {
-            type: 'ล้อ',
-            id: "A123",
-            company: 'kmitl',
-            amount: '100'
-        }
-    ]
+    data() {
+        return{
+            item:[]
+        };
+    },
+    methods:{
+        async getImport() {
+        const empData = await axios.get('http://localhost:3000/')
+        console.log(empData)
+        this.items = empData.data.data
+        console.log(this.items)
+      }
+    },
+    async mounted() {
+      await this.getImport()
+    }
+    // items: [
+    //     {
+    //         type: 'ล้อ',
+    //         id: "A123",
+    //         company: 'kmitl',
+    //         amount: '100'
+    //     }
+    // ]
 }
 </script>
 
--- SVAutotire/src/views/LogInPage.vue
+++ SVAutotire/src/views/LogInPage.vue
@@ -4,7 +4,7 @@
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
         SVAutotire Sale & Service    
       </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <form class="space-y-4 md:space-y-6" action="#">
                   <div>
@@ -24,12 +24,9 @@
                             <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
                   <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p>
+
               </form>
           </div>
       </div>
