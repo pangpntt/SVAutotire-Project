34c828b2590741279946cd7c2afc54978ba98088
Captain <archan.phalanuphat@gmail.com>
2024-03-13 12:50:09 +0700

    edit

--- SVAutotire/src/views/AddOrderPage.vue
+++ SVAutotire/src/views/AddOrderPage.vue
@@ -101,6 +101,8 @@ export default {
         })
         .then((res) => {
             console.log(res.status)
+            this.$swal('เพิ่มคำสั่งซื้อเสร็จสิ้น');
+            this.$router.push({ path: '/order' });
         })
         .catch((error) => {
             console.log(error.status)
@@ -110,8 +112,7 @@ export default {
         try {
             const response = await axios.get('http://13.213.12.136:3000/customer/all');
             this.oldCustomer = response.data.data;
-            this.$swal('เพิ่มคำสั่งซื้อเสร็จสิ้น');
-            this.$router.push({ path: '/order' });
+
         } catch (error) {
             console.error('Error fetching old customers:', error);
         }
